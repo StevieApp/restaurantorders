81b36c754e903b6278eafca633996c680d1ac32b
vERC254 <njorogesteve30@outlook.com>
2019-10-20 09:33:38 +0300

    need to figure out put api path to proceed... need to display items on cart...

--- app/src/main/java/com/example/restaurantorders/DispatchedOrders.java
+++ app/src/main/java/com/example/restaurantorders/DispatchedOrders.java
@@ -2,6 +2,7 @@ package com.example.restaurantorders;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +10,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 
 public class DispatchedOrders extends Fragment {
+
+    private ArrayList<Order> orderss = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,66 @@ public class DispatchedOrders extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dispatched_orders, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerviews);
+        final RecyclerViewAdapter adapter = new RecyclerViewAdapter(this.getContext(), orderss);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        String url = "https://demo.kilimanjarofood.co.ke/api/v1/dispatch/orders";
+        RequestQueue requestQueue = Volley.newRequestQueue(this.getContext());
+
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        Log.e("response from api", jsonObject.toString());
+                        System.out.println(jsonObject.toString());
+                        Log.d("response from api", "onResponse: \n"
+                                + jsonObject.toString());
+                        try {
+                            JSONObject j = jsonObject.getJSONObject("data");
+                            Log.d("response from api", j.toString());
+                            JSONArray rr = j.getJSONArray("orders");
+                            Log.d("response from api", rr.toString());
+                            Gson json = new Gson();
+                            Type types = new TypeToken<ArrayList<Order>>() {
+                            }.getType();
+                            ArrayList<Order> orders = json.fromJson(rr.toString(), types);
+                            for (int i = 0; i < orders.size(); i++) {
+                                Order g = orders.get(i);
+                                if (g.getDispatch_status() == 1) {
+                                    orderss.add(g);
+                                }
+                            }
+                            Order me = orders.get(1);
+                            Log.d("response from api", me.getName());
+                        } catch (JSONException e) {
+                            Log.d("response from api", "paaaapiiii");
+                            e.printStackTrace();
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.e("error from api", volleyError.toString());
+                        System.out.println(volleyError.toString());
+                        Log.d("error from api", "onErrorResponse: \n"
+                                + volleyError.toString());
+                    }
+                });
+        requestQueue.add(objectRequest);
     }
 
 }
--- app/src/main/java/com/example/restaurantorders/SpecificOrder.java
+++ app/src/main/java/com/example/restaurantorders/SpecificOrder.java
@@ -2,10 +2,30 @@ package com.example.restaurantorders;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SpecificOrder extends AppCompatActivity {
 
@@ -28,6 +48,8 @@ public class SpecificOrder extends AppCompatActivity {
             textpayno,
             textdispatchdate;
     Toolbar toolbar;
+    MaterialButton buttondispatch, buttoncanceldispatch;
+    Order ord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +75,8 @@ public class SpecificOrder extends AppCompatActivity {
         textpayref = findViewById(R.id.textpayref);
         textpayno = findViewById(R.id.textpayno);
         textdispatchdate = findViewById(R.id.textdispatchdate);
+        buttondispatch = findViewById(R.id.buttondispatch);
+        buttoncanceldispatch = findViewById(R.id.buttoncanceldispatch);
 
         specificorderid.setText(getIntent().getStringExtra("text"));
         toolbar.setTitle(getIntent().getStringExtra("text"));
@@ -80,5 +104,157 @@ public class SpecificOrder extends AppCompatActivity {
         textpayref.setText(getIntent().getStringExtra("textpayref"));
         textpayno.setText(getIntent().getStringExtra("textpayno"));
         textdispatchdate.setText(getIntent().getStringExtra("textdispatchdate"));
+
+
+        String orderid = getIntent().getStringExtra("text");
+        String orderidd = orderid.replace("#", "");
+        String url = "https://demo.kilimanjarofood.co.ke/api/v1/dispatch/order?orderId="
+                + orderidd;
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        final JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        //Log.e("response from api", jsonObject.toString());
+                        System.out.println(jsonObject.toString());
+                        //Log.d("response from api", "onResponse: \n"
+                        //       + jsonObject.toString());
+                        try {
+                            JSONObject j = jsonObject.getJSONObject("data");
+                            Log.d("response from api", j.toString());
+                            JSONObject rr = j.getJSONObject("orders");
+                            Log.d("response from api", rr.toString());
+                            Gson json = new Gson();
+                            Order me = json.fromJson(rr.toString(), Order.class);
+                            ord = me;
+                            textdeliveryplace.setText(me.getDelivery_address());
+                            textdeliverynumber.setText(String.valueOf(me.getDelivery_contact_phone_number()));
+                            String pending = "pending";
+                            String dispatched = "dispatched";
+                            if (me.getDispatch_status() == 0) {
+                                textdispatchstatus.setText(pending);
+                                textdispatchstatus.setTextColor(Color.GREEN);
+                                dispatchstatus.setText(pending);
+                                dispatchstatus.setTextColor(Color.GREEN);
+                            } else {
+                                textdispatchstatus.setText(dispatched);
+                                textdispatchstatus.setTextColor(Color.RED);
+                                dispatchstatus.setText(dispatched);
+                                dispatchstatus.setTextColor(Color.RED);
+                            }
+                            Log.d("response from api", me.getName());
+                            String dispatchtime;
+                            if (me.getDispatch_time() == null || me.getDispatch_time().equals("")) {
+                                dispatchtime = "Pending dispatch/Dispatch time not set";
+                                textdispatchdate.setText(dispatchtime);
+                            } else {
+                                textdispatchdate.setText(me.getDispatch_time());
+                            }
+                        } catch (JSONException e) {
+                            Log.d("response from api", "paaaapiiii");
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.e("error from api", volleyError.toString());
+                        System.out.println(volleyError.toString());
+                        Log.d("error from api", "onErrorResponse: \n"
+                                + volleyError.toString());
+                    }
+                }
+        );
+        requestQueue.add(objectRequest);
+
+
+        buttondispatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String orderid = getIntent().getStringExtra("text");
+                String orderidd = orderid.replace("#", "");
+                String url = "https://demo.kilimanjarofood.co.ke/api/v1/dispatch/order?orderId="
+                        + orderidd;
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                Date date = new Date();
+                String currentdate = formatter.format(date);
+                ord.setDispatch_status(1);
+                ord.setDispatch_time(currentdate);
+                Gson gson = new Gson();
+                String gee = gson.toJson(ord);
+                Log.d("loooooooooooooooming", gee);
+                JsonParser parser = new JsonParser();
+                JsonObject json = (JsonObject) parser.parse(gee);
+                JSONObject y;
+                try {
+                    y = new JSONObject(gee);
+                    Log.d("loooooooooooooooming", y.toString());
+                    JsonObjectRequest objectRequest = new JsonObjectRequest(
+                            Request.Method.PUT,
+                            url,
+                            y,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject jsonObject) {
+                                    //Log.e("response from api", jsonObject.toString());
+                                    System.out.println(jsonObject.toString());
+                                    //Log.d("response from api", "onResponse: \n"
+                                    //       + jsonObject.toString());
+                                /*try {
+                                    JSONObject j = jsonObject.getJSONObject("data");
+                                    Log.d("response from api", j.toString());
+                                    JSONObject rr = j.getJSONObject("orders");
+                                    Log.d("response from api", rr.toString());
+                                    Gson json = new Gson();
+                                    Order me = json.fromJson(rr.toString(), Order.class);
+                                    textdeliveryplace.setText(me.getDelivery_address());
+                                    textdeliverynumber.setText(String.valueOf(me.getDelivery_contact_phone_number()));
+                                    String pending = "pending";
+                                    String dispatched = "dispatched";
+                                    if(me.getDispatch_status() == 0){
+                                        textdispatchstatus.setText(pending);
+                                        textdispatchstatus.setTextColor(Color.GREEN);
+                                        dispatchstatus.setText(pending);
+                                        dispatchstatus.setTextColor(Color.GREEN);
+                                    }else{
+                                        textdispatchstatus.setText(dispatched);
+                                        textdispatchstatus.setTextColor(Color.RED);
+                                        dispatchstatus.setText(dispatched);
+                                        dispatchstatus.setTextColor(Color.RED);
+                                    }
+                                    Log.d("response from api", me.getName());
+                                } catch (JSONException e) {
+                                    Log.d("response from api", "paaaapiiii");
+                                    e.printStackTrace();
+                                }*/
+                                    Toast.makeText(SpecificOrder.this,
+                                            "Updated", Toast.LENGTH_SHORT).show();
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError volleyError) {
+                                    Log.e("error from api", volleyError.toString());
+                                    System.out.println(volleyError.toString());
+                                    Log.d("error from api", "onErrorResponse: \n"
+                                            + volleyError.toString());
+                                    Toast.makeText(SpecificOrder.this,
+                                            "Unable to update", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                    );
+                    requestQueue.add(objectRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
