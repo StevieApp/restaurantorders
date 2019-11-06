f6c330102d8685aed7858e80b328a10544331c5c
vERC254 <njorogesteve30@outlook.com>
2019-11-06 10:11:20 +0300

    firebase messaging intent

--- app/src/main/java/com/kilimanjarofood/restaurantorders/Fireb.java
+++ app/src/main/java/com/kilimanjarofood/restaurantorders/Fireb.java
@@ -1,5 +1,10 @@
 package com.kilimanjarofood.restaurantorders;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -11,16 +16,31 @@ public class Fireb extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
         notifier(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
     }
 
     public void notifier(String title, String message) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notif")
+        Intent intent = new Intent(this,
+                Viewing.class);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent
+                .getActivity(this,
+                        0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
+                "notif")
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.chicken)
                 .setAutoCancel(true)
-                .setContentText(message);
+                .setContentText(message)
+                .setLights(Color.MAGENTA, 6, 6)
+                .setContentIntent(pendingIntent);
+        Notification note = builder.build();
+        note.defaults |= Notification.DEFAULT_VIBRATE;
+        note.defaults |= Notification.DEFAULT_SOUND;
         NotificationManagerCompat how = NotificationManagerCompat.from(this);
-        how.notify(767, builder.build());
+        how.notify(767, note);
     }
 }
